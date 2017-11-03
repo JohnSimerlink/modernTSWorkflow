0f8ac242e7455df4b5b13f2f76e1871fcae6405b
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-03 11:42:46 -0700

    Step 7C: only send coverage report if coveralls yml exists

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 var gulp = require('gulp')
 var watch = require('gulp-watch')
 var exec = require('child_process').exec
@@ -8,11 +9,7 @@ var paths = {
 gulp.task('test', function() {
    exec('npm t', function(err, stdout, stderr){
        console.log(stdout)
-       console.log('Sending coverage report to coveralls.io')
-       exec('npm run coverage', function (err, stdout, stderr){
-           console.log(stdout)
-           console.log('Coverage report sent')
-       })
+       publishCoverageIfConfigExists()
    })
 })
 gulp.task('test-watch', function() {
@@ -21,3 +18,14 @@ gulp.task('test-watch', function() {
       gulp.start('test')
    })
 })
+function publishCoverageIfConfigExists(){
+    fs.open('.coveralls.yml', 'r', function (err, fd) {
+       if (!err){
+           console.log('Sending coverage report to coveralls.io')
+           exec('npm run coverage', function (err, stdout, stderr){
+               console.log(stdout)
+               console.log('Coverage report sent')
+           })
+       }
+    })
+}

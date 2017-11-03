fb51dcc490b32d1f0f63bf5fc1a60e97fc57891b
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-03 10:03:59 -0700

    Step 6D: Add 100% Coverage

--- test/joke.spec.js
+++ test/joke.spec.js
@@ -1,8 +1,14 @@
-import {chicken} from "../app/joke"
+import {chicken,stick} from "../app/joke"
 import {expect} from "chai"
 describe("Chicken", function() {
     it('Should be something about a road', function() {
         var containsRoad = chicken().indexOf("road") >= 0
         expect(containsRoad).to.equal(true)
+    })
+})
+describe("Stick", function() {
+    it('Should be something about a stick', function() {
+        var containsStick = stick().indexOf("stick") >= 0
+        expect(containsStick).to.equal(true)
     })
 })
